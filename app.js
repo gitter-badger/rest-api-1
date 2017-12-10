dce208071af489b134ad7728819ad5b40a05a244
Semyonic <semihonay@bilgiedu.net>
2017-12-10 17:55:32 +0300

    Update project

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-let opbeat = require('opbeat').start({
+const opbeat = require('opbeat').start({
   appId: 'f488d9f4a9',
   organizationId: 'bf09f7ac168f48adb9a7acce37ecfde0',
   secretToken: '841a590acc78fe16275fbb613956fc8871f0085f',
-});
+})
 
-let express = require('express');
-let path = require('path');
-let favicon = require('serve-favicon');
-let logger = require('morgan');
-let cookieParser = require('cookie-parser');
-let bodyParser = require('body-parser');
-let io = require('socket.io');
+const express = require('express')
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+const io = require('socket.io')
+const session = require('express-session')
 
 // Routes
-let index = require('./routes/index');
-let login = require('./routes/login');
-let users = require('./routes/users');
-let todos = require('./routes/todos');
-let news = require('./routes/news');
+const index = require('./routes/index')
+const login = require('./routes/login')
+const users = require('./routes/users')
+const todos = require('./routes/todos')
+const news = require('./routes/news')
 
 // load bunyan logger
-let bunyan = require('bunyan');
-let log = bunyan.createLogger({
+const bunyan = require('bunyan')
+const log = bunyan.createLogger({
   name: 'restAPI',
   level: bunyan.INFO,
   streams: [
@@ -34,48 +35,57 @@ let log = bunyan.createLogger({
       path: 'logs/app.json',
     },
   ],
-});
+})
 
 // load mongoose package
-let mongoose = require('mongoose');
+let mongoose = require('mongoose')
 
 // Use native Node promises
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
 // connect to MongoDB
-mongoose.connect('mongodb://admin:CgySm27C@ds163721.mlab.com:63721/todo').
+mongoose.connect('mongodb://admin:CgySm27C@ds129706.mlab.com:29706/api').
   then(() => {
-    log.info('MongoDB connected');
-    return console.log('connection succesful');
-  }).catch((err) => console.error(err));
+    log.info('MongoDB connected')
+    return console.log('connection succesful')
+  }).catch((err) => console.error(err))
 
-let app = express();
+const app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(opbeat.middleware.express());
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')))
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use(opbeat.middleware.express())
 
-app.use('/', index);
-app.use('/login', login);
-app.use('/users', users);
-app.use('/todos', todos);
-app.use('/news', news);
+app.use(session({ secret: 'test-session' }))
+
+/*let sess
+app.get('/', function (req, res) {
+  res.render('index2');
+  sess = req.session
+})*/
+
+app.use('/', index)
+app.use('/login', login)
+app.use('/users', users)
+app.use('/todos', todos)
+app.use('/news', news)
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 /*io.sockets.on('connection', (socket) => {
   let currentRoom;
@@ -113,25 +123,25 @@ app.use((req, res, next) => {
  */
 if (app.get('env') === 'development') {
   app.use((err, req, res, next) => {
-    res.status(err.status || 500);
+    res.status(err.status || 500)
     res.render('error', {
       message: err.message,
       error: err,
-    });
-    next();
-  });
+    })
+    next()
+  })
 }
 
 /**
  * production error handler no stacktraces leaked to user*
  */
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {},
-  });
-  next();
-});
+  })
+  next()
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
-let express = require('express');
-let router = express.Router();
+let express = require('express')
+let router = express.Router()
+let Fingerprint2 = require('fingerprintjs2')
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.render('index', { title: 'todo API semih' });
-});
+  res.render('index', { title: 'todo API semih' })
+})
 
-module.exports = router;
+router.post('/', (req, res) => {
+  console.log(req)
+})
+
+router.get('/public/js/finger.min.js', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/fingerprintjs2/dist/fingerprint2.min.js')
+})
+
+router.get('/public/css/bootstrap.min.css', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/bootswatch/dist/materia/bootstrap.min.css')
+})
+
+router.get('/public/js/jquery.min.js', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/jquery/dist/jquery.min.js')
+})
+
+router.get('/public/js/jquery.min.js.map', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/jquery/dist/jquery.min.js.map')
+})
+
+router.get('/public/js/bootstrap.min.js', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/bootstrap/dist/js/bootstrap.min.js')
+})
+
+router.get('/public/js/bootstrap.min.js.map', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/bootstrap/dist/js/bootstrap.min.js.map')
+})
+
+router.get('/public/js/popper.min.js', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/popper.js/dist/popper.min.js')
+})
+
+router.get('/public/js/popper.min.js.map', (req, res, next) => {
+  res.sendFile(process.cwd() + '/node_modules/popper.js/dist/popper.min.js.map')
+})
+
+module.exports = router
